98581a592f83e01f1c9cb492d06d32ead912e115
Qi Zhang <singularitti@outlook.com>
2018-10-27 14:32:12 -0400

    Change the implementation of `NamelistVariable`
    1. Add currying outer constructor
    2. Change `value` to be the last parameter

--- src/CoreDataStructure/Namelist.jl
+++ src/CoreDataStructure/Namelist.jl
@@ -13,9 +13,12 @@ module Namelist
 
 struct NamelistVariable{T}
     name::String
-    value::T
     in_namelist::String
+    value::T
 end
+
+NamelistVariable(name::String, in_namelist::String, value::T) where T = NamelistVariable{T}(name, in_namelist, value)
+NamelistVariable{T}(name::String, in_namelist::String) where T = value::T -> NamelistVariable(name, in_namelist, value)
 
 struct Namelist
     name::String
