9ced91557f3d87ce770c0cfec5f78f2287f29c7d
Qi Zhang <singularitti@outlook.com>
2018-10-27 15:12:23 -0400

    feat: Add `CONTROL_NAMELIST` and its variables

--- src/CoreDataStructure/Namelists.jl
+++ src/CoreDataStructure/Namelists.jl
@@ -25,5 +25,38 @@ struct Namelist
     values::Dict
 end
 
+const CONTROL_NAMELIST_VARIABLES = [
+    ("calculation", String),
+    ("title", String),
+    ("verbosity", String),
+    ("restart_mode", String),
+    ("wf_collect", Bool),
+    ("nstep", Int),
+    ("iprint", Int),
+    ("tstress", Bool),
+    ("tprnfor", Bool),
+    ("dt", Float64),
+    ("outdir", String),
+    ("wfcdir", String),
+    ("prefix", String),
+    ("lkpoint_dir", Bool),
+    ("max_seconds", Float64),
+    ("etot_conv_thr", Float64),
+    ("forc_conv_thr", Float64),
+    ("disk_io", String),
+    ("pseudo_dir", String),
+    ("tefield", Bool),
+    ("dipfield", Bool),
+    ("lelfield", Bool),
+    ("nberrycyc", Int),
+    ("lorbm", Bool),
+    ("lberry", Bool),
+    ("gdir", Int),
+    ("nppstr", Int),
+    ("lfcpopt", Bool),
+    ("gate", Bool)
+]
+
+const CONTROL_NAMELIST = Dict(name => NamelistVariable{type}(name, "CONTROL") for (name, type) in CONTROL_NAMELIST_VARIABLES)
 
 end
