3d0676acc6a912e96b1683ca28b725c2a410e5d2
Qi Zhang <singularitti@outlook.com>
2018-10-27 19:53:14 -0400

    feat: Add other cards

--- src/CoreDataStructure/Cards.jl
+++ src/CoreDataStructure/Cards.jl
@@ -19,4 +19,19 @@ struct AtomicSpecies <: Card
     values::Vector{Element}
 end
 
+struct CellParameters <: Card
+    option::String
+    values::Matrix{Float64}
+end
+
+struct AtomicPositions <: Card
+    option::String
+    values::Vector
+end
+
+struct KPoints <: Card
+    option::String
+    values
+end
+
 end
