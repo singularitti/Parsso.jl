feabfaaeb0b12f867fbb5d9e6d93df87b74488a7
Qi Zhang <singularitti@outlook.com>
2018-10-27 19:17:01 -0400

    feat: Finish Elements.jl temporarily

--- src/CoreDataStructure/Elements.jl
+++ src/CoreDataStructure/Elements.jl
@@ -12,8 +12,10 @@ julia>
 module Elements
 
 using FilePaths
+using JSON
 
-export Element
+export Element,
+    PeriodicTable
 
 struct Element
     symbol::String
@@ -21,7 +23,16 @@ struct Element
     pseudopotential::AbstractPath
 end
 
+Element(symbol, mass::Int, pseudopotential) = Element(symbol, convert(Float64, mass), pseudopotential)
 Element(symbol::String, mass::Float64) = pseudopotential::AbstractPath -> Element(symbol, mass, pseudopotential)
+Element(symbol::String, mass::Int) = Element(symbol, convert(Float64, mass))
 Element(symbol::String) = mass::Float64 -> Element(symbol, mass)
+
+open("data/PeriodicTableJSON.json") do f
+    d = JSON.parse(f)["elements"]
+    global PeriodicTable = Dict(
+    number => Element(element["symbol"], element["atomic_mass"]) for (number, element) in enumerate(d)
+)
+end
 
 end
