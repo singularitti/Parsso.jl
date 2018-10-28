341fad8f2756cfde36052877a9a8032b2c1a3b0b
Qi Zhang <singularitti@outlook.com>
2018-10-28 16:57:29 -0400

    Deprecate `PresetAtomicSpecies`, use `get_atomic_species` function
    Benefit:
    1. Separable kernel code
    2. Lazy generating elements (instead of generate all elements at once)
    3. Julia style code

--- src/CoreDataStructure/Elements.jl
+++ src/CoreDataStructure/Elements.jl
@@ -14,7 +14,8 @@ module Elements
 import PeriodicTable
 
 export AtomicSpecies,
-    PresetAtomicSpecies
+    get_element,
+    get_atomic_species
 
 struct AtomicSpecies
     symbol::String
@@ -25,8 +26,13 @@ end
 AtomicSpecies(symbol::String, mass::Float64) = pseudopotential::String -> AtomicSpecies(symbol, mass, pseudopotential)
 AtomicSpecies(symbol::String) = mass::Float64 -> AtomicSpecies(symbol, mass)
 
-const PresetAtomicSpecies = Dict(
-    element.name => AtomicSpecies(element.symbol, element.atomic_mass.val) for element in PeriodicTable.elements
-)
+get_element(index::Symbol) = PeriodicTable.elements.bysymbol[(Symbol ∘ uppercasefirst ∘ String)(index)]
+get_element(index::Int) = PeriodicTable.elements.bynumber[index]
+get_element(index::String) = PeriodicTable.elements.byname[lowercase(index)]
+
+function get_atomic_species(index::Union{Symbol, Int, String})
+    element = get_element(index)  # Separate kernel functions (aka, function barriers)
+    AtomicSpecies(element.symbol, element.atomic_mass.val)
+end
 
 end
