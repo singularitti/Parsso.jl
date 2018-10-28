c50b4791333f685fca0a55f798cc9b29e829234d
Qi Zhang <singularitti@outlook.com>
2018-10-28 16:25:36 -0400

    Rename `AtomicSpeciesFactory` to `PresetAtomicSpecies`

--- src/CoreDataStructure/Elements.jl
+++ src/CoreDataStructure/Elements.jl
@@ -14,7 +14,7 @@ module Elements
 import PeriodicTable
 
 export AtomicSpecies,
-    AtomicSpeciesFactory
+    PresetAtomicSpecies
 
 struct AtomicSpecies
     symbol::String
@@ -25,7 +25,7 @@ end
 AtomicSpecies(symbol::String, mass::Float64) = pseudopotential::String -> AtomicSpecies(symbol, mass, pseudopotential)
 AtomicSpecies(symbol::String) = mass::Float64 -> AtomicSpecies(symbol, mass)
 
-const AtomicSpeciesFactory = Dict(
+const PresetAtomicSpecies = Dict(
     element.name => AtomicSpecies(element.symbol, element.atomic_mass.val) for element in PeriodicTable.elements
 )
 
