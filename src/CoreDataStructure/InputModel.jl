4976461bee8c750dfa9fa34a8612ef1ec74d6520
Qi Zhang <singularitti@outlook.com>
2018-10-27 23:00:19 -0400

    Add QE programs and checking in inner constructor

--- src/CoreDataStructure/InputModel.jl
+++ src/CoreDataStructure/InputModel.jl
@@ -11,17 +11,47 @@ julia>
 """
 module InputModel
 
+const QE_PROGRAMS = (
+    "bands.x",
+    "bgw2pw.x",
+    "cp.x",
+    "cppp.x",
+    "dos.x",
+    "turbo_eels.x",
+    "turbo_lanczos.x",
+    "ld1.x",
+    "molecularpdos.x",
+    "neb.x",
+    "ph.x",
+    "pp.x",
+    "projwfc.x",
+    "pw.x",
+    "pw2bgw.x",
+    "pw2gw.x",
+    "pw_export.x",
+    "pwcond.x",
+    "turbo_spectrum.x"
+)
+
 abstract type AbstractInput end
 
 struct NamelistsOnlyInput <: AbstractInput
     program::String
     namelists::Vector{Namelist}
+    function NamelistsOnlyInput(program, namelists)
+        program in QE_PROGRAMS || throw(DomainError("The program $program is not recognized!"))
+        new(program, namelists)
+    end
 end
 
 struct NamelistsCardsInput <: AbstractInput
     program::String
     namelists::Vector{Namelist}
     cards::Vector{Card}
+    function NamelistsCardsInput(program, namelists, cards)
+        program in QE_PROGRAMS || throw(DomainError("The program $program is not recognized!"))
+        new(program, namelists, cards)
+    end
 end
 
 end
