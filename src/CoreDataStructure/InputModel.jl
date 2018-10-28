270a1045bae96809af1bb8948a92700b0399f3c0
Qi Zhang <singularitti@outlook.com>
2018-10-27 23:10:34 -0400

    Move QE defaults to Default.jl

--- src/CoreDataStructure/InputModel.jl
+++ src/CoreDataStructure/InputModel.jl
@@ -12,30 +12,7 @@ julia>
 module InputModel
 
 export NamelistsInput,
-    NamelistsCardsInput,
-    QE_PROGRAMS
-
-const QE_PROGRAMS = (
-    "bands.x",
-    "bgw2pw.x",
-    "cp.x",
-    "cppp.x",
-    "dos.x",
-    "turbo_eels.x",
-    "turbo_lanczos.x",
-    "ld1.x",
-    "molecularpdos.x",
-    "neb.x",
-    "ph.x",
-    "pp.x",
-    "projwfc.x",
-    "pw.x",
-    "pw2bgw.x",
-    "pw2gw.x",
-    "pw_export.x",
-    "pwcond.x",
-    "turbo_spectrum.x"
-)
+    NamelistsCardsInput
 
 abstract type AbstractInput end
 
